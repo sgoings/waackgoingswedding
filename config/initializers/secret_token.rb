f7525fbc2a8ea5e634941b22914ee2796b6f3e4f
Seth Goings <sethgoings@gmail.com>
2013-09-07 18:18:55 -0600

    Initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Waackgoingswedding::Application.config.secret_key_base = '6cd84bb02fef9ac460dadaa205a9706e0e265f60b7d31fa4399ec07b0b5ad7cfaf7164ff950ae21b54891ed3d4360a3ede1184fde5984b81e7bd58455a5c33f2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Waackgoingswedding::Application.config.session_store :cookie_store, key: '_waackgoingswedding_session'
