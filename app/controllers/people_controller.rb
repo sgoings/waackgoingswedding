855896e1ed6485cccd030414b224fac0d05d64da
Seth Goings <sethgoings@gmail.com>
2014-02-22 19:02:13 -0700

    Add mailer support

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -4,21 +4,41 @@ class PeopleController < ApplicationController
     @person = People.search_by_address(params[:address])
 
     if !params[:address].empty? && @person
-      render 'update'
+      redirect_to @person
     else
       flash[:search_status] = "Could not find address. Try again?"
       render "search"
     end
   end
 
+  def show
+    @person = People.find_by_address(params[:id])
+
+    render 'update'
+  end
+
   def update
     @person = People.find_by_address(params[:id])
     @person.update_attributes!(people_params)
-    flash[:update_status] = "Updated your RSVP status!"
+
+    begin
+      logger.info "Trying to send email #{@person.email} ..."
+      RsvpMailer.rsvp_email(@person, request.original_url).deliver
+      flash[:update_status] = "Updated your RSVP status! (sent an email, too)"
+    rescue
+      logger.info "Sending mail to #{@person.email} failed!"
+      flash[:update_status] = "Updated your RSVP status!"
+    end
+    
   end
 
   private
     def people_params
-      params.require(:people).permit(:attend, :email, :actual_adults, :actual_kids)
+      params.require(:people).permit(
+        :attend,
+        :email,
+        :actual_adults,
+        :actual_kids
+      )
     end
 end
---
+++ app/mailers/rsvp_mailer.rb
@@ -0,0 +1,11 @@
+class RsvpMailer < ActionMailer::Base
+  default from: "sethgoings@gmail.com"
+
+  def rsvp_email(person, url)
+    if(!person.email.nil? && !person.email.empty)
+        @person = person
+        @url  = url
+        mail(to: @person.email, subject: 'Thanks for RVSVP-ing at waackgoingswedding.com!')
+    end
+  end
+end
