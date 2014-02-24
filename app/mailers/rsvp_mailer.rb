3484c9902121e6257e83654832895df62595abc7
Seth Goings <sethgoings@gmail.com>
2014-02-23 21:39:20 -0700

    Add brittany

--- app/mailers/rsvp_mailer.rb
+++ app/mailers/rsvp_mailer.rb
@@ -2,10 +2,15 @@ class RsvpMailer < ActionMailer::Base
   default from: "sethgoings@gmail.com"
 
   def rsvp_email(person, url)
+    @person = person
+    @url  = url
+
+  	emails = ["sethgoings@gmail.com"]
     if(!person.email.nil? && !person.email.empty?)
-        @person = person
-        @url  = url
-        mail(to: @person.email, subject: 'Thanks for RVSVP-ing at waackgoingswedding.com!')
+    	emails << @person.email
     end
+
+    logger.info emails
+    mail(to: emails, bcc: "brittany.waack@gmail.com", subject: 'Thanks for RVSVP-ing at waackgoingswedding.com!')
   end
 end
